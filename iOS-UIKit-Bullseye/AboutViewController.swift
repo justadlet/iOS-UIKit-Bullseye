e3b24512e546c21036bebf3cadb764fb239c9d04
Adlet Zeineken <zeineken.adlet@gmail.com>
2020-12-09 20:59:03 +0600

    Added some style

---
+++ iOS-UIKit-Bullseye/AboutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutViewController.swift
+//  iOS-UIKit-Bullseye
+//
+//  Created by Adlet Zeineken on 09.12.2020.
+//  Copyright © 2020 justadlet. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func closePressed() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
